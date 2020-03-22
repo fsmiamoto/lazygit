e634d7c9eff6f96435b5d77dcc383b1fe7e0e5ba
Jesse Duffield <jessedduffield@gmail.com>
2020-03-22 15:15:53 +1100

    bump commit limit up to 300 and reset limit when switching branches or resetting

--- pkg/gui/reset_menu_panel.go
+++ pkg/gui/reset_menu_panel.go
@@ -21,6 +21,8 @@ func (gui *Gui) resetToRef(ref string, strength string, options commands.RunComm
 
 	go gui.refreshFiles()
 	go gui.refreshBranches()
+	// loading a heap of commits is slow so we limit them whenever doing a reset
+	gui.State.Panels.Commits.LimitCommits = true
 	gui.refreshCommits()
 	if err := gui.resetOrigin(gui.getCommitsView()); err != nil {
 		return err
